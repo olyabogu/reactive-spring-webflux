1cb3fce0121f7d2d6f402e1b368dd4ad2c77589f
Kroll, Olga <Olga.Kroll@bestbuy.com>
2023-08-30 16:41:46 -0500

    Build Reactive MicroServices using Spring WebFlux/SpringBoot

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,37 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+    private FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
 
+    @Test
+    void namesFlux() {
+        var namesFlux = service.namesFlux();
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben", "chloe")
+//                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        var namesFlux = service.namesFlux_map(3);
+        StepVerifier.create(namesFlux)
+                .expectNext("ALEX", "CHLOE")
+//                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        var namesFlux = service.namesFlux_immutability();
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben", "chloe")
+//                .expectNextCount(3)
+                .verifyComplete();
+    }
 }
