ab6dd52af79ab0bb83c8583e2d2e80e5a05fd54e
Kroll, Olga <Olga.Kroll@bestbuy.com>
2023-09-26 09:31:05 -0500

    Sections: Simple Non Blocking RESTFUL API using Annotated controller; Automated Tests using JUnit5 and @WebFluxTest

---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,50 @@
+package com.reactivespring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebFluxTest(controllers = FluxAndMonoController.class)
+public class FluxAndMonoControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void flux() {
+        webTestClient.get().uri("/flux").exchange().expectStatus().isOk().expectBodyList(Integer.class).hasSize(3);
+    }
+
+    @Test
+    void flux_2() {
+        var flux = webTestClient.get().uri("/flux").exchange().expectStatus().isOk().returnResult(Integer.class).getResponseBody();
+        StepVerifier.create(flux).expectNext(1, 2, 3).verifyComplete();
+    }
+
+    @Test
+    void flux_3() {
+        webTestClient.get().uri("/flux").exchange().expectStatus().isOk().expectBodyList(Integer.class).consumeWith(listEntityExchangeResult -> {
+            var responseBody = listEntityExchangeResult.getResponseBody();
+            assert Objects.requireNonNull(responseBody).size() == 3;
+        });
+    }
+
+    @Test
+    void mono() {
+        webTestClient.get().uri("/mono").exchange().expectStatus().isOk().expectBody(String.class).consumeWith(stringEntityExchangeResult -> {
+            var responseBody = stringEntityExchangeResult.getResponseBody();
+            assertEquals("hello world", responseBody);
+        });
+    }
+
+    @Test
+    void stream() {
+        var flux = webTestClient.get().uri("/stream").exchange().expectStatus().isOk().returnResult(Long.class).getResponseBody();
+        StepVerifier.create(flux).expectNext(0L,1L, 2L, 3L).thenCancel().verify();
+    }
+}
